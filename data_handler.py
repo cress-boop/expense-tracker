1f67876e2470cf5aba60901f4fa630b9b2688fd1
cress-boop <coligado.cristian01@gmail.com>
2026-05-05 23:05:06 +0800

    data_handler.py

--- data_handler.py
+++ data_handler.py
@@ -1,17 +1,19 @@
 import pandas as pd
 import os
 
-FILE_NAME = "expenses.csv"
+def get_file(username):
+    return f"{username}_expenses.csv"
 
-def load_data():
-    if os.path.exists(FILE_NAME):
-        return pd.read_csv(FILE_NAME)
+def load_data(username):
+    file = get_file(username)
+    if os.path.exists(file):
+        return pd.read_csv(file)
     return pd.DataFrame(columns=["Date", "Category", "Amount", "Description"])
 
-def save_data(df):
-    df.to_csv(FILE_NAME, index=False)
+def save_data(df, username):
+    df.to_csv(get_file(username), index=False)
 
-def add_expense(df, date, category, amount, description):
+def add_expense(df, username, date, category, amount, description):
     new_entry = pd.DataFrame([{
         "Date": date,
         "Category": category,
@@ -19,15 +21,15 @@ def add_expense(df, date, category, amount, description):
         "Description": description
     }])
     df = pd.concat([df, new_entry], ignore_index=True)
-    save_data(df)
+    save_data(df, username)
     return df
 
-def delete_expense(df, index):
+def delete_expense(df, username, index):
     df = df.drop(index).reset_index(drop=True)
-    save_data(df)
+    save_data(df, username)
     return df
 
-def update_expense(df, index, date, category, amount, description):
+def update_expense(df, username, index, date, category, amount, description):
     df.loc[index] = [date, category, amount, description]
-    save_data(df)
+    save_data(df, username)
     return df
